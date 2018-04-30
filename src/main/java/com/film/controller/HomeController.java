d23e142625386d8279cc7aeb286f04547bcca092
Nabeel Mujadidi <nabeel.94@live.com>
2018-04-30 05:11:42 +0100

    added runner class and two controller classes

---
+++ src/main/java/com/film/controller/HomeController.java
@@ -0,0 +1,14 @@
+package com.film.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+	
+	@RequestMapping("/")
+	public String home() {
+		return "Film Collection, reporting for duty";
+	}
+
+}
